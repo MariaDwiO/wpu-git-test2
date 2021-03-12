6594c73094d60a10e9833b5b44e0b3cf7afba055
maria dwi <oliviadwimaria@gmail.com>
2021-03-12 19:10:38 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ad, aliquid non voluptatum reprehenderit possimus error esse? Est, ad dolore rerum repudiandae ducimus eaque ea blanditiis nemo incidunt! Sunt, labore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
