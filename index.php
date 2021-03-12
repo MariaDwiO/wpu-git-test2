f6c248a6188ae68f5c7f9795104cd7548848b5bb
maria dwi <oliviadwimaria@gmail.com>
2021-03-12 19:02:27 +0700

    membuat file index.php & menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ad, aliquid non voluptatum reprehenderit possimus error esse? Est, ad dolore rerum repudiandae ducimus eaque ea blanditiis nemo incidunt! Sunt, labore.</p>
+</body>
+
+</html>
